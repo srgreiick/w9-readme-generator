690536a95f0f81e53c99fde3d062f6e7f3400fc4
sgreilick <samgreilick@gmail.com>
2020-07-26 12:33:13 -0600

    initialize with function, inquirer prompt

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//initializer
+function run() {
+    
+
 inquirer
 .prompt([
-{    type: "input",
+    {
+    type: "input",
     message: "Project Name",
-    name: "prodName"}
+    name: "prodName"
+    },
+    {    
+    type: "input",
+    message: "Project Description",
+    name: "prodDesc"
+    },
+    {    
+    type: "input",
+    message: "Project Table-Of-Contents",
+    name: "prodTOC"
+    },
+    {
+    type: "input",
+    message: "Project Description",
+    name: "prodDesc"
+    },
   ]);
  
+
+};
+
+run();
